969f7997b7c3100e6c1130bb5967eb307f3013d9
darwinsenior <darwinsenior@gmail.com>
2016-01-30 01:39:48 -0600

    added nicely the final commit for the hello

--- start.sh
+++ start.sh
@@ -14,8 +14,11 @@ if docker info; then
         --name firefly\
         -v $DIR/frontend:/www/static \
         -v $DIR/backend:/www/algorithm \
-        victorren/firefly_lsst&
-    docker exec firefly bash startup.sh
+        victorren/firefly_lsst:latest &
+    docker exec firefly ./startup.sh
+    PORT=ifconfig | egrep "^[a-z]|inet " | sed -e "s/ [ ]*Link.*/@/" -e "s/.*inet addr://" -e "s/ .*/#/" | tr -d '\012' | tr '@' ' ' | tr '#' '\012' | grep -e eth1 | awk '{ print $2 }'
+    echo visit the web with the following address $PORT:$1/static/index.html
+
 else
     echo "Please ssh into docker-machine"
 fi
