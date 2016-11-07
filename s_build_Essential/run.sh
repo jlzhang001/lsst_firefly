351e5777b2577749d1770043ea2460c2699e2566
Wei Ren <victor-ren@outlook.com>
2016-11-07 00:10:04 -0600

    docker image now uses Firefly (new API)

--- s_build_Essential/run.sh
+++ s_build_Essential/run.sh
@@ -1,8 +1,5 @@
 #! /bin/bash
 
 cd /opt/tomcat/webapps
-mkdir fftools && cd fftools && jar -xvf ../fftools.war
-sed -i -e '$ d' ./WEB-INF/config/app.prop
-echo "python.exe=/opt/conda/bin/python /www/algorithm/dispatcher.py" >> ./WEB-INF/config/app.prop
-echo "helloworld"
+echo "Starting TOMCAT server......"
 bash /opt/tomcat/bin/startup.sh
